b2d61dced374a9def0303ea0f72964d32c72a960
aamrdit <aamir.butt@outlook.com>
2022-06-22 01:11:53 +0100

    new importing modules scripts are added'

---
+++ dog.py
@@ -0,0 +1,15 @@
+# Owning functions
+
+def bark(pet = 'A Dog') :
+         print(pet, 'Says WOOF!')   
+          
+def lick(pet = 'A Dog') :
+          print(pet, 'Drinks water')
+          
+def nap(pet = 'A Dog') :
+          print(pet, 'Sleeps in the Sun')
+          
+          
+       
+
+
